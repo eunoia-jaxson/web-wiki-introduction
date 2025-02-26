2513c50a8f5cf579f819be1a305112e9baa3c560
eunoia-jaxson <eunoia@handong.ac.kr>
2025-02-26 16:33:58 +0900

    feat: 댓글 입력 input text 구현

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
   window.scrollTo(0, 0);
   new WOW().init();
+
+  const textArea = document.querySelector('#comment-writing');
+
+  window.handleCancel = () => {
+    if (
+      textArea.value != '' &&
+      window.confirm('댓글 작성을 취소하시겠습니까?')
+    ) {
+      textArea.value = '';
+    }
+  };
+
+  window.handleRegister = () => {
+    if (textArea.value != '' && window.confirm('댓글을 등록하시겠습니까?')) {
+      textArea.value = '';
+    }
+  };
 });
